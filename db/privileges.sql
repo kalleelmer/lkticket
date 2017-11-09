aaa86b82ea842ab0b313083adcb4de26bef1c59c
Kalle Elmér <kalle@silvertejp.nu>
2017-11-09 20:58:52 +0100

    Privilegier styr inte host.

--- db/privileges.sql
+++ db/privileges.sql
@@ -1,25 +1,25 @@
 USE `lkticket`;
 
-GRANT SELECT ON `profiles` TO 'lkticket'@'localhost';
+GRANT SELECT ON `profiles` TO 'lkticket';
 
-GRANT SELECT, INSERT(`name`), UPDATE(`name`) ON `shows` TO 'lkticket'@'localhost';
+GRANT SELECT, INSERT(`name`), UPDATE(`name`) ON `shows` TO 'lkticket';
 
-GRANT SELECT, INSERT(`name`), UPDATE(`name`) ON `users` TO 'lkticket'@'localhost';
+GRANT SELECT, INSERT(`name`), UPDATE(`name`) ON `users` TO 'lkticket';
 
-GRANT SELECT ON `user_profiles` TO 'lkticket'@'localhost';
+GRANT SELECT ON `user_profiles` TO 'lkticket';
 
-GRANT SELECT, INSERT(`token`,`user_id`) ON `user_tokens` TO 'lkticket'@'localhost';
+GRANT SELECT, INSERT(`token`,`user_id`) ON `user_tokens` TO 'lkticket';
 
-GRANT SELECT, INSERT(`show_id`, `start`) ON `performances` TO 'lkticket'@'localhost';
+GRANT SELECT, INSERT(`show_id`, `start`) ON `performances` TO 'lkticket';
 
-GRANT SELECT, INSERT(`show_id`, `name`, `ticketCount`), UPDATE(`ticketCount`) ON `categories` TO 'lkticket'@'localhost';
+GRANT SELECT, INSERT(`show_id`, `name`, `ticketCount`), UPDATE(`ticketCount`) ON `categories` TO 'lkticket';
 
-GRANT SELECT, INSERT(`show_id`, `name`) ON `rates` TO 'lkticket'@'localhost';
+GRANT SELECT, INSERT(`show_id`, `name`) ON `rates` TO 'lkticket';
 
-GRANT SELECT, INSERT, UPDATE(`active_ticket_id`) ON `seats` TO 'lkticket'@'localhost';
+GRANT SELECT, INSERT, UPDATE(`active_ticket_id`) ON `seats` TO 'lkticket';
 
-GRANT SELECT, INSERT(`category_id`, `rate_id`, `price`), DELETE ON `prices` TO 'lkticket'@'localhost';
+GRANT SELECT, INSERT(`category_id`, `rate_id`, `price`), DELETE ON `prices` TO 'lkticket';
 
-GRANT SELECT, INSERT(`expires`, `identifier`) ON `orders` TO 'lkticket'@'localhost';
+GRANT SELECT, INSERT(`expires`, `identifier`) ON `orders` TO 'lkticket';
 
-GRANT SELECT, INSERT (`order_id`, `seat_id`, `rate_id`, `price`) ON `tickets` TO 'lkticket'@'localhost';
+GRANT SELECT, INSERT (`order_id`, `seat_id`, `rate_id`, `price`) ON `tickets` TO 'lkticket';
