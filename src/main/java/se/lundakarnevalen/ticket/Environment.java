e8b88ac6a46d3af860cfc5bd928b2994c134c498
Kalle Elmér <kalle@silvertejp.nu>
2017-11-09 20:24:44 +0100

    Konfiguration använder miljövariabler i första hand.

--- src/main/java/se/lundakarnevalen/ticket/Environment.java
+++ src/main/java/se/lundakarnevalen/ticket/Environment.java
@@ -5,15 +5,17 @@ import javax.naming.NamingException;
 
 public class Environment {
 	public static String getProperty(String key) {
+		String env = System.getProperty(key);
+		if (env != null) {
+			return env;
+		}
 		try {
 			InitialContext initialContext = new javax.naming.InitialContext();
 			String value = (String) initialContext.lookup("java:comp/env/" + key);
-			if (value != null) {
-				return value;
-			}
+			return value;
 		} catch (NamingException e) {
 			e.printStackTrace();
+			return null;
 		}
-		return System.getProperty(key);
 	}
 }
