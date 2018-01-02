5dcf3b0d91c09220f4acb9e972396084563b2cb0
Kalle Elmér <kalle@silvertejp.nu>
2018-01-02 17:39:38 +0100

    Anrop för admin som hämtar platser på en föreställning.

--- src/main/java/se/lundakarnevalen/ticket/api/PublicPerformances.java
+++ src/main/java/se/lundakarnevalen/ticket/api/PublicPerformances.java
@@ -1,8 +1,6 @@
 package se.lundakarnevalen.ticket.api;
 
 import java.sql.SQLException;
-import java.util.List;
-
 import javax.annotation.security.PermitAll;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -13,7 +11,6 @@ import javax.ws.rs.core.Response;
 import org.json.JSONException;
 import se.lundakarnevalen.ticket.api.Request;
 import se.lundakarnevalen.ticket.db.Performance;
-import se.lundakarnevalen.ticket.db.Seat;
 
 @Path("/public/performances")
 @PermitAll
@@ -26,14 +23,5 @@ public class PublicPerformances extends Request {
 		Performance perf = Performance.getSingle(id);
 		assertNotNull(perf, 404);
 		return status(200).entity(perf).build();
-	}
-
-	@GET
-	@Path("/{id}/seats")
-	public Response getSeats(@PathParam("id") long id) throws SQLException, JSONException {
-		Performance perf = Performance.getSingle(id);
-		assertNotNull(perf, 404);
-		List<Seat> seats = Seat.getByPerformance(perf.id);
-		return status(200).entity(seats).build();
 	}
 }
--- src/main/java/se/lundakarnevalen/ticket/api/admin/AdminPerformances.java
+++ src/main/java/se/lundakarnevalen/ticket/api/admin/AdminPerformances.java
@@ -1,13 +1,26 @@
 package se.lundakarnevalen.ticket.api.admin;
 
+import java.sql.SQLException;
+import java.util.List;
+
 import javax.annotation.security.RolesAllowed;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
 import se.lundakarnevalen.ticket.api.PublicPerformances;
+import se.lundakarnevalen.ticket.db.Seat;
 
 @Path("/admin/performances")
 @RolesAllowed("ADMIN")
 @Produces("application/json; charset=UTF-8")
 public class AdminPerformances extends PublicPerformances {
+	@GET
+	@Path("/{id}/seats")
+	public Response getSeats(@PathParam("id") int id) throws SQLException {
+		List<Seat> seats = Seat.getByPerformance(id);
+		return status(200).entity(seats).build();
+	}
 }
